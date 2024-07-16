cdf5b907bc6786c580336c1eba66856cb8132455
Tatsuya Ishikawa <34103899+tatsuyai713@users.noreply.github.com>
2024-07-16 17:10:53 +0900

    Update update_headers.sh

--- scripts/update_headers.sh
+++ scripts/update_headers.sh
@@ -5,38 +5,44 @@ camel_to_snake() {
     echo "$input" | sed -E 's/([A-Z]+)([A-Z][a-z])/\1_\2/g' | sed -E 's/([a-z\d])([A-Z])/\1_\2/g' | tr '[:upper:]' '[:lower:]'
 }
 
-directory="."
+folder_path="$(pwd)"
+echo "Start processing in $folder_path"
 
-find "$directory" -type f -name "*.h" | while read -r file; do
-    if [[ "$file" != *"PubSubTypes.h"* ]]; then
-        base_name=$(basename "$file" .h)
+find "$folder_path" -type f -name "*.h" | while read -r file_path; do
+    echo "Found $file_path"
+    if [[ "$file_path" != *"PubSubTypes.h"* ]]; then
 
-        parent_dir="$(dirname "${file}")"
-        parent_dir_name="$(basename "${parent_dir}")"
+        file_name=$(basename $file_path)
 
-        grandparent_dir="$(dirname "${parent_dir}")"
-        grandparent_dir_name="$(basename "${grandparent_dir}")"
-
-        pubsubtypes_file="${base_name}PubSubTypes.h"
-        snake_case=$(camel_to_snake "$file")
+        class_name=$(basename $file_name | cut -d'.' -f1)
         
-        if ! grep -q "#include \"$pubsubtypes_file\"" "$file"; then
-            # echo "#include \"fast_dds_header.hpp\"" >> "$file"
-            echo "#include \"$pubsubtypes_file\"" >> "$file"
+        file_content=$(cat $file_path)
 
-            sed -i '/^class '${base_name}'$/ {
-N
-s/^class '${base_name}'/class '${base_name}' : public std::enable_shared_from_this<'${base_name}'>/
-N
-a\
-\ \ \ \ using SharedPtr = std::shared_ptr<'${base_name}'>;
-            }' "$file"
-            
-            cp "$file" "${snake_case}pp"
+        file_content=$(echo "$file_content" | sed '/#include/ i\
+#include <memory>' | sed '0,/#include <memory>/! {/#include <memory>/d}')
 
-        else
-            echo "#include \"$pubsubtypes_file\" already exists in $file"
+        modified_line=$(echo "$file_content" | grep -n "#include \"${class_name}PubSubTypes.h\"")
+        echo "Modified line: $modified_line"
+
+        if [ -n "$modified_line" ]; then
+            echo "The file $file_name has already been modified. Skipping..."
+            continue
         fi
+
+        modified_content=$(echo "$file_content" | sed "s/class $class_name/class $class_name : public std::enable_shared_from_this<$class_name>/")
+
+        modified_content_with_include=$(echo "$modified_content" | sed "$ a #include \"${class_name}PubSubTypes.h\"")
+
+        modified_content_with_insert=$(echo "$modified_content_with_include" | awk -v insert_code="            using SharedPtr = std::shared_ptr<${class_name}>;" '/class '$class_name'/ {p=1} p && /public:/ {print; print insert_code; p=0} 1')
+
+        echo "$modified_content_with_insert" > $file_path
+        snake_name=$(camel_to_snake $class_name)
+        directory_name=$(dirname $file_path)
+
+        cp $file_path $directory_name/$snake_name.hpp
+
+        echo "Modified the file $file_name"
+
     fi
 done
 
